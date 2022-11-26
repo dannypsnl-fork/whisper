c994d7a14b9cb257e86accd0ea0b69385c9234af
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-11-26 14:03:52 +0800

    improve surface syntax
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- main.rkt
+++ main.rkt
@@ -10,7 +10,7 @@
    #:args ()
    ; let Nat : U = (N : U) -> (N -> N) -> N -> N;
    (define tm (parse
-               #'(let id : ((A : U) -> (A -> A)) = (lam A (lam x x))
+               #'(let id : ((A : U) -> (A -> A)) = (lam A x => x)
                    in (id U))))
    (define ty (infer-empty tm))
    (printf "type of term:~n~n~a~n~nis~n~n~a~n" tm (readback '() ty))))
